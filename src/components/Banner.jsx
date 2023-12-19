22490cb84c0c34ab832faac95325b60599e49b2a
learnwithancor <ancorpoint0@gmail.com>
2023-12-19 23:14:15 +0600

    commited

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,9 @@
-import banner from '../assets/Banner/banner.jpg'
+import banner from '../assets/Banner/bannerImg.jpg'
 
 const Banner = () => {
     return (
-        <div className='sm:h-[120vh sm:h-full h-[50vh] sm:mt-0 mt-16'>
-            <img className='w-full h-full object-cover' src={banner} alt="" />
+        <div className='md:h-[130vh] sm:h-[80vh] h-[60vh] sm:pt-0 pt-16'>
+            <img className='w-full h-full object-cover object-bottom' src={banner} alt="" />
             <div className=' h-[150vh]'>
             </div>
         </div>
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -5,6 +5,7 @@ import { CiMenuFries } from "react-icons/ci";
 
 const Header = () => {
     const [color, setColor] = useState(true);
+    const [isActive, setIsActive] = useState(false);
 
     const changeColor = () => {
       if (window.scrollY <= 0) {
@@ -20,16 +21,21 @@ const Header = () => {
             <div className=' flex m:justify-start justify-between  items-center h-16 max-width'>
             <div className='flex gap-x-3 items-center'>
             <img className='rounded-full w-12 h-12' src={logo} alt="logo" />
-            <h1 className='text-[18px] font-semibold sm:text-inherit text-black'>Himu <span>Amin</span></h1>
+            <h1 className='sm:text-xl text-sm font-bold sm:text-inherit text-black font-mono'>Himu <span>Amin</span></h1>
             </div>
             <nav>
-                <ul className='md:flex hidden gap-x-10 sm:text-inherit text-black'>
+                <ul className='md:flex hidden gap-x-10 sm:text-inherit text-black font-mono'>
                     <li>Home</li>
                     <li>Blogs</li>
                     <li>About</li>
                     <li>Contact</li>
                 </ul>
-                <CiMenuFries className='md:hidden text-2xl sm:text-inherit text-black'/>
+                {/* <CiMenuFries className='md:hidden text-2xl sm:text-inherit text-black'/> */}
+                <div onClick={() => {setIsActive(!isActive)}} className='hamburger md:hidden'>
+
+      <div className={`burger ${isActive ? 'burgerActive' : ""}`}></div>
+
+    </div>
             </nav>
             </div>
         </header>
